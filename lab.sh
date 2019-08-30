fbca2ed4b61858f30f0fae87d3a3afe1c3c9c531
Loris <mrnlrs.tor@gmail.com>
2019-08-30 10:38:04 +1000

    Adds secrets and cleans unnecessary files

--- lab.sh
+++ lab.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# Fires Jupyter Lab inside the local docker container.
+# Port is 8889. Mapping is defined in docker-compose.yml
 jupyter lab \
 	--notebook-dir="$JUPYTER_HOME_IMAGE" \
 	--no-browser \
